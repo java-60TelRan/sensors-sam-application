b88008f408cc075ac9ffd7abae5d2eb4411145da
Yuragranovsky <yuragranovsky@gmail.com>
2026-02-08 22:07:44 +0200

    delete from index unneeded files

---
+++ sensors-stack/.aws-sam/cache/39672e71-ffa8-45ab-9283-28bcf3a5f97a/app.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import os
+from zoneinfo import ZoneInfo
+from logger import logger
+import json
+def processMessage(messageDict: dict, tz_name: str):
+    
+    sensorId, value, max_value, dt = messageDict["sensorId"],messageDict["value"]\
+    , messageDict["limit"], datetime.fromtimestamp(messageDict["timestamp"], tz=ZoneInfo(tz_name))
+    print(f"sensorId = {sensorId}\nvalue = {value}\nmax-value={max_value}\ndate-time={dt}")
+  
+def processRecord(record, tz_name):
+    message = record["Sns"]["Message"]
+    logger.debug(f"message from SNS record is {message}")
+    messageDict: dict = json.loads(message)
+    processMessage(messageDict, tz_name)
+    
+def lambda_handler(event, context):
+    try:
+        tz_name = os.getenv('TZ', 'Asia/Jerusalem')
+        for record in event["Records"]:
+            processRecord(record, tz_name)
+    except Exception as e:
+        logger.error(f"Error: {str(e)}")
+        raise e        
+    
